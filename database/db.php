28d205526fceea0df9d7341dadb7d106732d2b26
Rizqi Kevin Octavian <kevin.octavian@delameta.com>
2025-08-02 17:17:00 +0700

    change db

--- database/db.php
+++ database/db.php
@@ -1,14 +1,16 @@
 <?php
-$servername = "db2";
-$username = "learning_user";
-$password = "learning_pass";
-$dbname = "dblearning";
+// db.php
+$host = 'db2';  // Ganti dengan host database Anda, misalnya 'localhost' atau IP
+$dbname = 'dblearning';     // Nama database Anda
+$username = 'learning_user';   // Username database, biasanya 'root' untuk XAMPP
+$password = 'learning_pass';       // Password untuk username database, kosongkan jika tidak ada
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+try {
+    // Membuat koneksi PDO
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);  // Menampilkan error jika ada masalah
+} catch (PDOException $e) {
+    echo "Koneksi gagal: " . $e->getMessage();
+    exit();
 }
 ?>
